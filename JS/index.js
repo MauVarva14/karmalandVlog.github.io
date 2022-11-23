53b2a4c0f292263660f87b529723f9996c0d32e2
MauVarva14 <118914100+MauVarva14@users.noreply.github.com>
2022-11-23 17:57:52 -0600

    Creo que ya

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+const navToggle = document.querySelector(".toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("arial-label", "Close menu");
+    } else {
+        navToggle.setAttribute("arial-label", "Open menu");
+    }
+
+});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+  }
+  
---
+++ JS/javascript.js
@@ -0,0 +1,1 @@
+alert('De click a los logotipos de twitch y Youtube para redirigirse')
